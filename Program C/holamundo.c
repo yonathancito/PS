ccfbe87701a034bda2bd4000ec1a46b973910b9b
yonathan <yhona777@gmail.com>
2019-04-04 10:13:43 -0500

    ver4

--- Program C/holamundo.c
+++ Program C/holamundo.c
@@ -7,8 +7,16 @@ actualizar "abajo"*/
 /*./holamundo.exe*/
 #include <stdio.h>
 int main(){
-	printf("Hola mssundooooooooooooo\n");
-	printf("Hola aaaaas\n");
+	printf("Hola\n");
+	int numero1;
+
+	printf("De un valor:");
+	scanf("%d",&numero1);
+
+	printf("El valor del numero1 es",numero1);
+
+
+
 	return 0;
 }
 
