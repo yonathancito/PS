f266fdcd660484f592b74a7db159c67e1083bca2
yonathan <yhona777@gmail.com>
2019-04-04 09:51:04 -0500

    ver3

--- Program C/holamundo.c
+++ Program C/holamundo.c
@@ -1,7 +1,14 @@
+/*tridente
+mensaje ->
+"+"
+control + enter
+actualizar "abajo"*/
+
+/*./holamundo.exe*/
 #include <stdio.h>
 int main(){
 	printf("Hola mssundooooooooooooo\n");
-	printf("Hola aaaaa\n");
+	printf("Hola aaaaas\n");
 	return 0;
 }
 
